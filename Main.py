53c3fa557a64bf249e1be3bf6ca93594c300c6aa
bjuncklaus <bjuncklaus@gmail.com>
2016-02-22 21:53:42 -0500

    all implemented except why

--- Main.py
+++ Main.py
@@ -1,24 +1,33 @@
+"""
+Author: Bruno Juncklaus Martins
+Computing ID: bjm3j
+"""
 import re
 
-rootVariables = {}
+rootVariables = []
+rootValues = {}
+learnedFacts = {}
 variablesValues = {}
+variablesValuesFake = {}
 learnedVariables = {}
-rules = {}
+learnedVariablesFake = {}
+reasoning = []
+rules = []
 symbols = ["!", "&", "|"]
 
 parser = re.compile('[a-zA-Z_]+')
+parser2 = re.compile('[!&|a-zA-Z->]+')
 
 def teachNew(var, string):
-    if (var in rootVariables):
-        print("The variable has already been defined.")
+    if (var in [j for i in rootVariables for j in i]):
         return None
     else:
-        rootVariables[var] = string
+        rootVariables.append({var : string})
         variablesValues[var] = False
+        variablesValuesFake[var] = False
 
 def teachExistent(var, bool):
     if (var not in variablesValues):
-        print("The variable", var, "needs to be defined first. Use TEACH <ARG> <VAR> = <STRING>.")
         return None
     else:
         if (bool == "true"):
@@ -26,35 +35,172 @@ def teachExistent(var, bool):
         if (bool == "false"):
             value = False
         variablesValues[var] = value
+        variablesValuesFake[var] = value
 
 def teachRule(rule, var):
-    if (var not in rootVariables):
-        print("The variable", var, "needs to be defined first. Use TEACH <ARG> <VAR> = <STRING>.")
+    if (var not in [j for i in rootVariables for j in i]):
+        return None
 
     if(isValid(rule)):
-        rules[rule] = var
+        rules.append({rule : var})
 
-def learn():
-    print()
+def learn(query):
+    for dict in rules:
+        for rule in dict:
+            individualRules = parser.findall(rule)
+            newRule = rule
 
+            rulesUsedToLearn = []
+            for r in individualRules:
+                if (query == False):
+                    newRule = newRule.replace(r, str(variablesValues[r]))
+                else:
+                    newRule = newRule.replace(r, str(variablesValuesFake[r]))
+                    rulesUsedToLearn.append(rootValues[r])
+
+            learnedFacts[dict[rule]] = rulesUsedToLearn
+            evaluate = newRule.replace("!", " not ").replace("|", " or ").replace("&", " and ")
+            boolValue = eval(evaluate)
+
+            if (query == False):
+                global learnedVariablesFake
+                if (boolValue == True & (dict[rule] not in learnedVariables)):
+                    learnedVariables[dict[rule]] = rule
+                    learnedVariablesFake = learnedVariables
+                    teachExistent(dict[rule], "true")
+                    learn(False)
+            else:
+                if (boolValue == True & (dict[rule] not in learnedVariablesFake)):
+                    learnedVariablesFake[dict[rule]] = rule
+                    variablesValuesFake[dict[rule]] = True
+                    learn(True)
+
+def query(q):
+    for root in rootVariables:
+        for dict in root:
+            rootValues[dict] = root[dict]
+
+    if (isValid(q)):
+        variablesInQuery = parser.findall(q)
+        if (len(variablesInQuery) > 1):
+            factsTemp = {}
+            ret = True
+            for vq in variablesInQuery:
+                if (variablesValuesFake[vq] == False):
+                    ret = False
+                else:
+                    factsTemp[vq] = vq
+
+                learn(True)
+                for learned in learnedVariablesFake:
+                    factsTemp[learned] = learnedVariablesFake[learned]
+
+                for vq in variablesInQuery:
+                    q = q.replace(vq, str(variablesValuesFake[vq]))
+
+                newRule = q.replace("!", " not ").replace("|", " or ").replace("&", " and ")
+
+                for vq in variablesInQuery:
+                    if (vq in learnedFacts):
+                        reasoning.append({"K" : vq})
+                        reasoning.append({"B" : learnedFacts[vq]})
+                    reasoning.append({"C" : rootValues[vq]})
+                reasoning.append({"V" : eval(newRule)})
+
+                return eval(newRule)
+        else:
+            if (variablesValuesFake[variablesInQuery[0]] == False):
+                learn(True)
+                for vq in variablesInQuery:
+                    q = q.replace(vq, str(variablesValuesFake[vq]))
+
+                newRule = q.replace("!", " not ").replace("|", " or ").replace("&", " and ")
+
+                for vq in variablesInQuery:
+                    if (vq in learnedFacts):
+                        for l in learnedVariablesFake:
+                            if (l != variablesInQuery[0] and l in learnedFacts):
+                                reasoning.append({"K" : learnedFacts[l]})
+                                reasoning.append({"B" : learnedFacts[l]})
+
+                        if (vq not in learnedVariablesFake):
+                            reasoning.append({"K" : vq})
+                        reasoning.append({"B" : learnedFacts[vq]})
+                    else:
+                        reasoning.append({"K" : vq})
+
+                reasoning.append({"C" : rootValues[variablesInQuery[0]]})
+                reasoning.append({"V" : eval(newRule)})
+                return eval(newRule)
+            else:
+                for vq in variablesInQuery:
+                    q = q.replace(vq, str(variablesValuesFake[vq]))
+
+                newRule = q.replace("!", " not ").replace("|", " or ").replace("&", " and ")
+
+                reasoning.append({"K" : variablesInQuery[0]})
+                reasoning.append({"C" : rootValues[variablesInQuery[0]]})
+                reasoning.append({"V" : eval(newRule)})
+
+                return eval(newRule)
+
+    return (q in learnedVariablesFake)
+
+def why(q):
+    print(query(q))
+
+    known = []
+    because = []
+    conclusion = []
+
+    for dict in reasoning:
+        for item in dict:
+            if (item == "K"):
+                if (isinstance(dict[item], list)):
+                    for v in dict[item]:
+                        if (v not in known):
+                            known.append(v)
+                else:
+                    if (rootValues[dict[item]] not in known):
+                        known.append(rootValues[dict[item]])
+            if (item == "B"):
+                if (isinstance(dict[item], list)):
+                    for v in dict[item]:
+                        if (v not in because):
+                            because.append(v)
+                else:
+                    if (rootValues[dict[item]] not in because):
+                        because.append(rootValues[dict[item]])
+            if (item == "C"):
+                conclusion.append(dict[item])
+            if (item == "V"):
+                if (dict[item] == False):
+                    conclusion[0] = "THUS I CANNOT PROVE " + conclusion[0]
+                else:
+                    conclusion[0] = "I THUS KNOW THAT " + conclusion[0]
+
+    print("I KNOW THAT", known, "BECAUSE", known, "I KNOW THAT", because, conclusion)
 
 def isValid(string):
     variablesInRule = parser.findall(string)
     for var in variablesInRule:
-        if (var not in rootVariables):
-            print("The variable", var, "needs to be defined first. Use TEACH <ARG> <VAR> = <STRING>.")
+        if (var not in [j for i in rootVariables for j in i]):
             return False
     return True
 
-def list():
+def listt():
     print("Root Variables:")
     for v in rootVariables:
-        print("\t", v, "=", rootVariables[v])
+        for r in v:
+            print("\t", r, "=", v[r])
     print()
 
     print("Learned Variables:")
     for v in learnedVariables:
-        print("\t", v, "=", learnedVariables[v])
+        for dict in rootVariables:
+            for dictValue in dict:
+                if (dictValue == v):
+                    print("\t", v, "=", dict[dictValue])
     print()
 
     print("Facts:")
@@ -65,95 +211,33 @@ def list():
 
     print("Rules:")
     for v in rules:
-        print("\t", v, "->", rules[v])
+        for r in v:
+            print("\t", r, "->", v[r])
 
 
+def main():
+    while (True):
+        command = input("")
+        individualCommand = parser2.findall(command)
 
+        if ("Teach" in individualCommand):
+            if ("-R" in individualCommand):
+                teachNew(individualCommand[2], individualCommand[3])
+            elif ("->" in individualCommand):
+                teachRule(individualCommand[1], individualCommand[3])
+            else:
+                teachExistent(individualCommand[1], individualCommand[2])
 
-"""
-Tests
-"""
-def testTeachNew():
-    def test1(function, param1, param2):
-        print("Testing", function.__name__, "with", str(param1), "and", str(param2))
-        function(param1, param2)
-        assert len(rootVariables) == 1
-        assert len(variablesValues) == 1
-        assert variablesValues[param1] == False
-        print("All tests passed!")
+        if ("List" in individualCommand):
+            listt()
 
-    test1(teachNew, "S", "a")
-    test1(teachNew, "S", "a")
+        if ("Learn" in individualCommand):
+            learn(False)
 
-    def test2(function, param1, param2):
-        print("Testing", function.__name__, "with", str(param1), "and", str(param2))
-        function(param1, param2)
-        assert len(rootVariables) == 2
-        assert len(variablesValues) == 2
-        assert variablesValues[param1] == False
-        print("All tests passed!")
+        if ("Query" in individualCommand):
+            print(query(individualCommand[1]))
 
-    test2(teachNew, "B", "b")
+        if ("Why" in individualCommand):
+            why(individualCommand[1])
 
-testTeachNew()
-
-def testTeachExistent():
-    def test1(function, param1, param2):
-        print("Testing", function.__name__, "with", str(param1), "and", str(param2))
-        function(param1, param2)
-        assert len(variablesValues) == 2
-        assert variablesValues["S"] == False
-        assert variablesValues["B"] == False
-        print("All tests passed!")
-
-    test1(teachExistent, "S", "false")
-
-    def test2(function, param1, param2):
-        print("Testing", function.__name__, "with", str(param1), "and", str(param2))
-        function(param1, param2)
-        assert len(variablesValues) == 2
-        assert variablesValues["S"] == True
-        assert variablesValues["B"] == False
-
-    test2(teachExistent, "S", "true")
-
-testTeachExistent()
-
-def testTeachRule():
-    def test1(function, param1, param2):
-        print("Testing", function.__name__, "with", str(param1), "and", str(param2))
-        function(param1, param2)
-        assert len(rules) == 0
-        print("All tests passed!")
-
-    test1(teachRule, "(BANANA&ABACAXI)|!MORANGO", "B")
-
-    def test2(function, param1, param2):
-        print("Testing", function.__name__, "with", str(param1), "and", str(param2))
-        function(param1, param2)
-        assert len(rules) == 1
-        assert rules[param1] == param2
-        print("All tests passed!")
-
-    test2(teachRule, "!S", "B")
-
-testTeachRule()
-
-def testIsValid():
-    def test1(function, param1):
-        print("Testing", function.__name__, "with", str(param1))
-        assert function(param1) == False
-        print("All tests passed!")
-
-    test1(isValid, "BICICLET_A")
-
-    def test2(function, param1):
-        print("Testing", function.__name__, "with", str(param1))
-        assert function(param1) == True
-        print("All tests passed!")
-
-    test2(isValid, "B|!S")
-
-testIsValid()
-
-list()
+main()
